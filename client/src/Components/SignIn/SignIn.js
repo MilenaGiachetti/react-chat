14efe9cf8e7e893bbc6ee78ea4efe24db09ff470
Milena Giachetti <giachettimilena@gmail.com>
2021-03-30 20:28:59 -0300

    Change to relative units

--- client/src/Components/SignIn/SignIn.js
+++ client/src/Components/SignIn/SignIn.js
@@ -14,20 +14,24 @@ const SignIn = (props) => {
 			}}
 			className={classes.SignInCtn}
 		>
-			<p>Enter username:</p>
-			<input
-				type="text"
-				placeholder={'Enter username or email'}
-				value={username}
-				onChange={e => setUsername(e.target.value)}
-			/>
-			<input
-				type="password"
-				placeholder={'Enter password'}
-				value={password}
-				onChange={e => setPassword(e.target.value)}
-			/>
-			<input type="submit" value='Enter' />
+			<label>Username or email
+				<input
+					type="text"
+					placeholder={'Enter username or email'}
+					value={username}
+					onChange={e => setUsername(e.target.value)}
+				/>
+			</label>
+			<label>Password
+				<input
+					type="password"
+					placeholder={'Enter password'}
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+				/>
+				<input type="submit" value='Enter' />
+			</label>
+			<a>Don't have an account? Sign up</a>
 		</form>
 	)
 }
